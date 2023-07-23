61e75ec3842d312901e10f5af52ce8e706e45df5
Will Mear <williammear@yahoo.com>
2023-07-23 18:01:07 +0000

    hide api

--- code-converter-backend/src/main/java/com/willmear/codeconverter/config/RestTemplateConfig.java
+++ code-converter-backend/src/main/java/com/willmear/codeconverter/config/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package com.willmear.codeconverter.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
